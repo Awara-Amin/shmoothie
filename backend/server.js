38b6528d2fce51bbfb0708602b6e72fb324b442c
AaA <awarageochemistry@gmail.com>
2025-07-04 19:30:28 +0300

    admin froentend url added

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,58 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import { connectDB } from './config/db.js'
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import { connectDB } from "./config/db.js"
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path"
+import { fileURLToPath } from "url"
 
-import userRouter from './routes/userRoute.js'
-import cartRouter from './routes/cartRoute.js'
-import itemRouter from './routes/itemRoute.js';
-import orderRouter from './routes/orderRoute.js';
+import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
+import itemRouter from "./routes/itemRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
-const app = express();
-const port = process.env.PORT || 4000;
+const app = express()
+const port = process.env.PORT || 4000
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-// MIDDLEWARE 
+// MIDDLEWARE
 app.use(
-    cors({
-        origin: (origin, callback) => {
-            const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true,
-    })
-);
+  cors({
+    origin: (origin, callback) => {
+      // const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+      const allowedOrigins = [
+        "https://shmoothie-frontend.onrender.com",
+        "https://shmoothie-admin.onrender.com",
+      ]
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true,
+  })
+)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // DB CONNECT
-connectDB();
+connectDB()
 
 // Routes
-app.use('/api/user', userRouter)
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api/cart', cartRouter)
-app.use('/api/items', itemRouter);
-app.use('/api/orders', orderRouter);
+app.use("/api/user", userRouter)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+app.use("/api/cart", cartRouter)
+app.use("/api/items", itemRouter)
+app.use("/api/orders", orderRouter)
 
-app.get('/', (req, res) => {
-    res.send('API WORKING');
+app.get("/", (req, res) => {
+  res.send("API WORKING")
 })
 
 app.listen(port, () => {
-    console.log(`Server Started on http://localhost:${port}`)
+  console.log(`Server Started on http://localhost:${port}`)
 })
